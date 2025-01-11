097adcef41a5e34bde55b1efa099b20bd67d5a45
Joy43 <ssjoy43@gmail.com>
2025-01-11 08:44:38 +0600

    dynamic router setup

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,73 +1,20 @@
 import { Layout, Menu, MenuProps } from 'antd';
-import { Children } from 'react';
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { createElement } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
-
+import { adminPaths, adminSidebarItems } from '../../routes/admin.routes';
+import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
+import Sidebar from './Sidebar';
 const { Header, Content, Footer, Sider } = Layout;
-const items=[
-  {key:'1',
-    label:<NavLink to="/admin/dashboard">Dashboard</NavLink>
-  },
-  {key:'2',
-    label:'Users'
-  },
-  {key:'3',
-    label:'Products'
-  },
-  {key:'4',
-    label:'Orders'
-  },
-  {key:'5',
-    label:'about'
-  },
-  {
-    key:'5',
-    label:'User Management',
-    children:[
-      {
-       key:'11',
-       label:'create admin'
-      },
-      {
-        key:'23',
-        label:'create student'
-      }
-    ]
-  }
-  
-]
+
 const MainLayout = () => {
   return (
     <Layout style={{ height: '100vh' }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-
-            height: '4rem',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <h1>Univesity</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['4']}
-          items={items}
-          
-        />
-      </Sider>
+      <Sidebar />
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: '24px 16px 0' }}>
@@ -80,9 +27,6 @@ const MainLayout = () => {
             <Outlet />
           </div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>
-          Ant Design ©{new Date().getFullYear()} Created by Ant UED
-        </Footer>
       </Layout>
     </Layout>
   );
--- src/utils/routesGenerator.ts
+++ src/utils/routesGenerator.ts
@@ -1,35 +1,25 @@
-import { ReactNode } from "react";
+import { TRoute, TUserPath } from '../types';
 
-type TRoute = {
-  path: string;
-  element: ReactNode;
+export const routeGenerator = (items: TUserPath[]) => {
+  const routes = items.reduce((acc: TRoute[], item) => {
+    if (item.path && item.element) {
+      acc.push({
+        path: item.path,
+        element: item.element,
+      });
+    }
+
+    if (item.children) {
+      item.children.forEach((child) => {
+        acc.push({
+          path: child.path!,
+          element: child.element,
+        });
+      });
+    }
+
+    return acc;
+  }, []);
+
+  return routes;
 };
-
-type TUserPath={
-    name:string,
-    path:string,
-    element:ReactNode;
-    children:TUserPath[]
-}
-
-export const routeGenerator=(items:TUserPath[])=>{
-    const routes = items.reduce((acc: TRoute[], item: TUserPath) => {
-        if (item.path && item.element) {
-            acc.push({
-                path: item.path,
-                element: item.element,
-            });
-        }
-
-        if (item.children) {
-            item.children.forEach((child: TUserPath) => {
-                acc.push({
-                    path: child.path,
-                    element: child.element,
-                });
-            });
-        }
-        return acc;
-    }, []);
- return routes
-}
