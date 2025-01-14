264b58e6310edf311f82ee1d4abf523378df5a77
Joy43 <ssjoy43@gmail.com>
2025-01-15 00:42:23 +0600

    aoi redux setup applied

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,21 +1,46 @@
+import { Button } from "antd"
 import { useForm } from "react-hook-form"
+import { useLoginMutation } from "../redux/features/auth/authApi"
+import Password from "antd/es/input/Password"
 
 
 const login = () => {
-  const{register,handleSubmit}=useForm()
-  const onSubmit=(data)=>{
-    console.log(data)
+  const{register,handleSubmit}=useForm({
+    defaultValues:{
+      userId:'A-002',
+      password:'admin123'
+    }
+  })
+  interface FormData {
+    userId: string;
+    password: string;
   }
+
+  interface UserInfo {
+    id: string;
+    password: string;
+  }
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    const userInfo: UserInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
+  const [login,{data,error}]=useLoginMutation();
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form  className="max-auto" onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="id">ID</label>
+        <label htmlFor="id"{...register('userId')}>ID</label>
         <input type="text"id="id"/>
       </div>
       <div>
-        <label htmlFor="password">ID</label>
-        <input type="text"id="password"/>
+        <label htmlFor="password">password</label>
+        <input type="text"id="password" {...register('password')}/>
       </div>
+      <Button htmlType="submit" >Resgister</Button>
     </form>
   )
 }
--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
+import { baseApi } from '../api/baseApi'
 // ...
 const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]:baseApi.reducer,
 auth:authReducer,
 
 
   },
+
+  middleware:getDefaultMiddlewares=>getDefaultMiddlewares().concat(baseApi.middleware)
 })
 export type RootState = ReturnType<typeof store.getState>
 
-export default store
+export default store;
